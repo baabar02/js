462569283a05b2f0203a4fec107cd0fe42d80263
baabar02 <baabarmx@gmail.com>
2025-02-19 20:46:21 -0800

    asdads

---
+++ homework.js
@@ -0,0 +1,137 @@
+let number1 = 3;
+if (number1 % 2 === 1) {
+  console.log("ogogdson too sondgoi:", number1);
+} else {
+  console.log("ogogdson too tegsh:", number1);
+}
+
+if (number1 % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+let nums = [3, 2, 7, 11];
+
+for (let i = 0; i < 4; i++) {
+  if (nums % 2 === 0) {
+    console.log("even", nums[i]);
+  } else {
+    console.log("odd", nums[i]);
+  }
+}
+let j = 0;
+while (j < 4) {
+  if (nums[j] % 2 === 0) {
+    console.log("even", nums[j]);
+  } else {
+    console.log("odd", nums[i]);
+  }
+  j++;
+}
+
+let number2 = 2;
+if (number2 % 2 === 1) {
+  console.log("ogogdson too sondgoi:", number2);
+} else {
+  console.log("ogogdson too tegsh:", number2);
+}
+
+let number3 = 11;
+if (number2 % 2 === 1) {
+  console.log("ogogdson too sondgoi:", number3);
+} else {
+  console.log("ogogdson too tegsh:", number3);
+}
+
+let number4 = 7;
+if (number4 % 2 === 1) {
+  console.log("ogogdson too sondgoi:", number4);
+} else {
+  console.log("ogogdson too tegsh:", number4);
+}
+
+let diga = 9;
+let digb = 3;
+let digc = 2;
+
+if (diga < digb && diga < digc) {
+  console.log("baga too1:", diga);
+} else {
+  console.log("ikh too1", diga);
+}
+if (digb < digc && digc < digb) {
+  console, log("baga too2:", digb);
+} else {
+  console.log("baga too3:", digc);
+}
+
+// for ( let diga, diga <= digabc, diga++);
+let dig1 = 7;
+let dig2 = 1;
+let dig3 = 12;
+
+if (dig1 > dig2 && dig1 > dig3) {
+  console.log("ikh too1:", dig1);
+} else {
+}
+
+let tooa = 3;
+let toob = 6;
+let tooc = 9;
+
+// if (too1 < toob && toob < tooc ) {
+//         console.log ("hamgiin baga too:", too1) ;
+//     } else if (tooc < tooa && toob < tooa);{
+//         console.log ("hamgiin baga too :", tooa){
+//     }else if (tooc < tooa && tooc < toob) {
+//             console.log ("hamgiin baga too:", toob)
+//     else if (tooc < tooa && tooc < tooc)
+//         console.log ("hamgiin baga too:", tooc);
+//         }
+//     }
+let n1 = 2;
+let n2 = 9;
+let n3 = 4;
+
+let urjver = 0;
+
+if (n1 > 5) {
+  n1 = 1;
+}
+if (n2 > 5) {
+  n2 = 1;
+}
+if (n3 > 1) {
+  n3 = 1;
+}
+console;
+// urjver = n1 * n2 * n3 ;
+// urjver = urjver * n3;s
+console.log("urjver", urjver);
+// let urjver = 1;
+
+let sar = 9;
+
+if (sar === 12 || sar === 1 || sar === 2) {
+  console.log("ovol bn");
+} else if (sar === 3 || sar === 4 || sar === 5) {
+  console.log("havar bn");
+} else if (sar === 6 || sar === 7 || sar === 8) {
+  console.log("zun bn");
+} else if (sar === 9 || sar === 10 || sar === 11) {
+  console.log("namar bn");
+} else {
+  console.log("tanii oruulsan sariin too buruu bn.");
+}
+
+
+let numTotal = [1,2,3...100];
+
+for (let i = 0; i < 4; i++) {
+  if (nums % 2 === 0) {
+    console.log("even", nums[i]);
+  } else {
+    console.log("odd", nums[i]);
+  }
+}

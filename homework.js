f22950935063003fc8a3d7078e20c384482d4e36
baabar02 <baabarmx@gmail.com>
2025-02-23 18:01:41 +0800

    js

--- homework.js
+++ homework.js
@@ -1,240 +1,293 @@
-let number1 = 3;
-if (number1 % 2 === 1) {
-  console.log("ogogdson too sondgoi:", number1);
-} else {
-  console.log("ogogdson too tegsh:", number1);
-}
+// let number1 = 3;
+// if (number1 % 2 === 1) {
+//   console.log("ogogdson too sondgoi:", number1);
+// } else {
+//   console.log("ogogdson too tegsh:", number1);
+// }
 
-if (number1 % 2 === 0) {
-  console.log("even");
-} else {
-  console.log("odd");
-}
+// if (number1 % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
 
-let nums = [3, 2, 7, 11];
+// let nums = [3, 2, 7, 11];
 
-for (let i = 0; i < 4; i++) {
-  if (nums % 2 === 0) {
-    console.log("even", nums[i]);
-  } else {
-    console.log("odd", nums[i]);
-  }
-}
-let j = 0;
-while (j < 4) {
-  if (nums[j] % 2 === 0) {
-    console.log("even", nums[j]);
-  } else {
-    console.log("odd", nums[j]);
-  }
-  j++;
-}
+// for (let i = 0; i < 4; i++) {
+//   if (nums % 2 === 0) {
+//     console.log("even", nums[i]);
+//   } else {
+//     console.log("odd", nums[i]);
+//   }
+// }
+// let j = 0;
+// while (j < 4) {
+//   if (nums[j] % 2 === 0) {
+//     console.log("even", nums[j]);
+//   } else {
+//     console.log("odd", nums[j]);
+//   }
+//   j++;
+// }
 
-let number2 = 2;
-if (number2 % 2 === 1) {
-  console.log("ogogdson too sondgoi:", number2);
-} else {
-  console.log("ogogdson too tegsh:", number2);
-}
+// let number2 = 2;
+// if (number2 % 2 === 1) {
+//   console.log("ogogdson too sondgoi:", number2);
+// } else {
+//   console.log("ogogdson too tegsh:", number2);
+// }
 
-let number3 = 11;
-if (number3 % 2 === 1) {
-  console.log("ogogdson too sondgoi:", number3);
-} else {
-  console.log("ogogdson too tegsh:", number3);
-}
+// let number3 = 11;
+// if (number3 % 2 === 1) {
+//   console.log("ogogdson too sondgoi:", number3);
+// } else {
+//   console.log("ogogdson too tegsh:", number3);
+// }
 
-let number4 = 7;
-if (number4 % 2 === 1) {
-  console.log("ogogdson too sondgoi:", number4);
-} else {
-  console.log("ogogdson too tegsh:", number4);
-}
+// let number4 = 7;
+// if (number4 % 2 === 1) {
+//   console.log("ogogdson too sondgoi:", number4);
+// } else {
+//   console.log("ogogdson too tegsh:", number4);
+// }
 
-let diga = 9;
-let digb = 3;
-let digc = 2;
-let digmin ;
+// let diga = 9;
+// let digb = 3;
+// let digc = 2;
+// let digmin ;
 
-if (diga <= digb && diga <= digc) {
-  digmin = diga;
-} else if (digb <= diga && digb <= digc) {
-  digmin = digb ;    
-} else if ( digc <= diga && digc <= digb)
-  digmin = digc ;
+// if (diga <= digb && diga <= digc) {
+//   digmin = diga;
+// } else if (digb <= diga && digb <= digc) {
+//   digmin = digb ;    
+// } else if ( digc <= diga && digc <= digb)
+//   digmin = digc ;
 
-  console.log ('bodlogo-1: hamgiin baga too :', digmin) ;
-
-
-let dig1 = 7;
-let dig2 = 1;
-let dig3 = 12;
-let digmax = 0;
-
-if (dig1 >= dig2 && dig1 >= dig3) {
-  digmax = dig1 ;
-} else if (dig2 >= dig1 && dig2 >= dig3 ){
-  digmax = dig2 ;
-} else if (dig3 >= dig1 && dig3 >= dig2) {
-  digmax = dig3 ;
-} 
-
-console.log ( 'bodlogo-2 : hamgiin ikh too :', digmax ) ; 
+//   console.log ('bodlogo-1: hamgiin baga too :', digmin) ;
 
 
-let tooa = 3;
-let toob = 6;
-let tooc = 9;
+// let dig1 = 7;
+// let dig2 = 1;
+// let dig3 = 12;
+// let digmax = 0;
 
-if (tooa < toob && tooa < tooc ) {
-  console.log ("bodlogo-1 bagsh, hamgiin baga too:", tooa) ;
-} else if (toob < tooa && toob < tooc) {;
-  console.log ("hamgiin baga too :", toob) ;
-} else if (tooc < tooa && tooc < toob) {
-  console.log ("hamgiin baga too:", tooc) ; 
-}
+// if (dig1 >= dig2 && dig1 >= dig3) {
+//   digmax = dig1 ;
+// } else if (dig2 >= dig1 && dig2 >= dig3 ){
+//   digmax = dig2 ;
+// } else if (dig3 >= dig1 && dig3 >= dig2) {
+//   digmax = dig3 ;
+// } 
 
-let n1 = 2;
-let n2 = 9;
-let n3 = 4;
-let urjver = 1;
-
-if (n1 < 5) {
-  urjver *= n1;
-}
-if (n2 < 5) {
-  urjver *= n2;
-}
-if (n3 < 5) {
-  urjver *= n3;
-} console.log ("urjver", urjver);
+// console.log ( 'bodlogo-2 : hamgiin ikh too :', digmax ) ; 
 
 
+// let tooa = 3;
+// let toob = 6;
+// let tooc = 9;
+
+// if (tooa < toob && tooa < tooc ) {
+//   console.log ("bodlogo-1 bagsh, hamgiin baga too:", tooa) ;
+// } else if (toob < tooa && toob < tooc) {;
+//   console.log ("hamgiin baga too :", toob) ;
+// } else if (tooc < tooa && tooc < toob) {
+//   console.log ("hamgiin baga too:", tooc) ; 
+// }
+
+// let n1 = 2;
+// let n2 = 9;
+// let n3 = 4;
+// let urjver = 1;
+
+// if (n1 < 5) {
+//   urjver *= n1;
+// }
+// if (n2 < 5) {
+//   urjver *= n2;
+// }
+// if (n3 < 5) {
+//   urjver *= n3;
+// } console.log ("urjver", urjver);
 
 
-let sar = 9;
-
-if (sar === 12 || sar === 1 || sar === 2) {
-  console.log("ovol bn");
-} else if (sar === 3 || sar === 4 || sar === 5) {
-  console.log("havar bn");
-} else if (sar === 6 || sar === 7 || sar === 8) {
-  console.log("zun bn");
-} else if (sar === 9 || sar === 10 || sar === 11) {
-  console.log("namar bn");
-} else {
-  console.log("tanii oruulsan sariin too buruu bn.");
-}
 
 
-let numTotal = [1,2,3];
+// let sar = 9;
 
-for (let i = 0; i < 4; i++) {
-  if (nums % 2 === 0) {
-    console.log("even", nums[i]);
-  } else {
-    console.log("odd", nums[i]);
-  }
-}
-
-let numa = 11 ;
-let numb = 9 ; 
-let numc = 4 ;
-let numd = 3 ; 
-let mulmax = 1 ;
-
-if (numa > 5) {
-  mulmax *= numa ;
-}
-if (numb > 5) {
-  mulmax *= numb ;
-}
-if (numc > 5) {
-  mulmax *= numb 
-}
-if (numd > 5) {
-  mulmax *= numd
-}
-console.log ( '5 aas ikh toonii urjver :', mulmax )
-
-let bat = 67 ;
-let ochir = 59 ;
-
-if ( bat >=59 &&  bat < 60) {
-  console.log ('muu' , bat) ;
-} else if (bat >= 60 && bat < 70 ) {
-  console.log ('hangalttai' , bat) ;
-}
-
-if (ochir >= 59 && ochir < 60) {
-  console.log ('muu' , ochir) ;
-} else if ('hangalttai' , ochir ) ;
-
-const birthYears = [1994, 1997, 2003, 1993, 2001];
-let currentYear = 2025 ;
-ages = [];
-
-for ( let i = 0; i < birthYears.length; i++) {
-  ages.push(currentYear-birthYears[i]);
-} console.log('nas', ages);
+// if (sar === 12 || sar === 1 || sar === 2) {
+//   console.log("ovol bn");
+// } else if (sar === 3 || sar === 4 || sar === 5) {
+//   console.log("havar bn");
+// } else if (sar === 6 || sar === 7 || sar === 8) {
+//   console.log("zun bn");
+// } else if (sar === 9 || sar === 10 || sar === 11) {
+//   console.log("namar bn");
+// } else {
+//   console.log("tanii oruulsan sariin too buruu bn.");
+// }
 
 
-const numbers = [1, 2, 3, 4];
+// let numTotal = [1,2,3];
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i] = numbers[i] * 2;
-} console.log('2 oor urjuulsen hariu:', numbers);
+// for (let i = 0; i < 4; i++) {
+//   if (nums % 2 === 0) {
+//     console.log("even", nums[i]);
+//   } else {
+//     console.log("odd", nums[i]);
+//   }
+// }
 
-const numbera = [11, 22, 33, 44];
-sumeven = 0;
-for ( let i = 0;  i < numbera.length; i++) {
-  if (numbera[i] % 2 === 0) {
-    sumeven += numbera[i];
-  }
-} console.log ('array, tegsh toonii niilber:', sumeven);
+// let numa = 11 ;
+// let numb = 9 ; 
+// let numc = 4 ;
+// let numd = 3 ; 
+// let mulmax = 1 ;
+
+// if (numa > 5) {
+//   mulmax *= numa ;
+// }
+// if (numb > 5) {
+//   mulmax *= numb ;
+// }
+// if (numc > 5) {
+//   mulmax *= numb 
+// }
+// if (numd > 5) {
+//   mulmax *= numd
+// }
+// console.log ( '5 aas ikh toonii urjver :', mulmax )
+
+// let bat = 67 ;
+// let ochir = 59 ;
+
+// if ( bat >=59 &&  bat < 60) {
+//   console.log ('muu' , bat) ;
+// } else if (bat >= 60 && bat < 70 ) {
+//   console.log ('hangalttai' , bat) ;
+// }
+
+// if (ochir >= 59 && ochir < 60) {
+//   console.log ('muu' , ochir) ;
+// } else if ('hangalttai' , ochir ) ;
+
+// const birthYears = [1994, 1997, 2003, 1993, 2001];
+// let currentYear = 2025 ;
+// ages = [];
+
+// for ( let i = 0; i < birthYears.length; i++) {
+//   ages.push(currentYear-birthYears[i]);
+// } console.log('nas', ages);
 
 
-let sumTot = 0;
+// const numbers = [1, 2, 3, 4];
 
-for (let i = 0; i < numbers.length; i++) {
-  sumTot += numbera[i] ;
-} console.log ('buh toonii niilberuud', sumTot) ;
+// for (let i = 0; i < numbers.length; i++) {
+//   numbers[i] = numbers[i] * 2;
+// } console.log('2 oor urjuulsen hariu:', numbers);
 
-let avar = 0; 
+// const numbera = [11, 22, 33, 44];
+// sumeven = 0;
+// for ( let i = 0;  i < numbera.length; i++) {
+//   if (numbera[i] % 2 === 0) {
+//     sumeven += numbera[i];
+//   }
+// } console.log ('array, tegsh toonii niilber:', sumeven);
 
-for (let i = 0; i < numbera.length; i++) {
- if (avar += numbera[i] / 4 );
-} console.log('dundaj:', avar);
 
-const array1 = Array.from({length:100}, (_,i )=> i+1);
-let arrayeven = 0;
-for (let i = 0; i <array1.length; i++) {
-  if (array1[i] %2 === 0) {
-  arrayeven += array1[i] ;
-  }
-} console.log ('100 hurtelh toonii tegsh:' , arrayeven);
+// let sumTot = 0;
 
-let arrayodd = 0; 
+// for (let i = 0; i < numbers.length; i++) {
+//   sumTot += numbera[i] ;
+// } console.log ('buh toonii niilberuud', sumTot) ;
 
-for (let i = 0; i < array1.length; i++) {
-  if(array1[i] % 2 === 1) {
-    arrayodd += array1[i] ;
-  }
-} console.log ('100 hurtelh toonii sondgoi niilberuud', arrayodd);
+// let avar = 0; 
 
-const arrayn = Array.from({length:200}, (_,i) => i+1);
-let multeven = 1; 
+// for (let i = 0; i < numbera.length; i++) {
+//  if (avar += numbera[i] / 4 );
+// } console.log('dundaj:', avar);
 
-for (let i = 0; i < arrayn.length; i++) {
-  if (arrayn[i] % 2 === 0) {
-      multeven *= arrayn[i] ;
-  }
-} console.log('n tegsh toonii niilber:', multeven);
+// const array1 = Array.from({length:100}, (_,i )=> i+1);
+// let arrayeven = 0;
+// for (let i = 0; i <array1.length; i++) {
+//   if (array1[i] %2 === 0) {
+//   arrayeven += array1[i] ;
+//   }
+// } console.log ('100 hurtelh toonii tegsh:' , arrayeven);
 
-let evenCount = 0;
+// let arrayodd = 0; 
 
-for (let i = 0; i < arrayn.length; i++) {
-  if (arrayn[i] % 2 === 0) {
-    evenCount++ ;  
-  }
-} console.log( 'tegsh toonii shirheg:', evenCount); 
+// for (let i = 0; i < array1.length; i++) {
+//   if(array1[i] % 2 === 1) {
+//     arrayodd += array1[i] ;
+//   }
+// } console.log ('100 hurtelh toonii sondgoi niilberuud', arrayodd);
+
+// const arrayn = Array.from({length:200}, (_,i) => i+1);
+// let multeven = 1; 
+
+// for (let i = 0; i < arrayn.length; i++) {
+//   if (arrayn[i] % 2 === 0) {
+//       multeven *= arrayn[i] ;
+//   }
+// } console.log('n tegsh toonii niilber:', multeven);
+
+// let evenCount = 0;
+
+// for (let i = 0; i < arrayn.length; i++) {
+//   if (arrayn[i] % 2 === 0) {
+//     evenCount++ ;  
+//   }
+// } console.log( 'tegsh toonii shirheg:', evenCount); 
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log("1-100 tegsh", i);
+//   }
+// }
+ 
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 1) {
+//     console.log("1-100 sondgoi", i);
+//   }
+// }
+ 
+// let multiple = 1;
+// for (let n = 1; n < 20; n++) {
+//   if (n % 2 === 0);
+//   multiple = multiple * n;
+// }
+ 
+// console.log("asdn:", multiple);
+ 
+// let x = 1234;
+// let digit = 0;
+// let s = 0;
+// let count = 0;
+// while (x !== 0) {
+//   digit = x % 10;
+//   s = s + digit;
+//   x = (x - digit) / 10;
+//   count++;
+//   console.log("d:", digit);
+//   console.log("s:", s);
+//   console.log("x:", x);
+//   console.log("sum:", count);
+//   console.log("==========");
+// }
+
+let x = 1234;
+let digit = 0;
+
+while ( x !== 0) {
+  digit = x % 10 ;
+} console.log ( 'urvuu too' , digit);
+
+// const arr = [1, 2, 3, 4, 5];
+// let arrtot = 0;
+// for (let j = 0; j < arr.length; j++) {
+//   arrtot = arrtot + arr[j];
+ 
+//   console.log("niilber", arrtot);
+//   // arr[j] = arr[j] * 2;
+//   // console.log("original", arr[j]);
+// }
+
---
+++ homework2.js
@@ -0,0 +1,59 @@
+let x = 1234;
+let digit = 0 ;
+let s = 0 ;
+while ( x !== 0) {
+
+    digit = x % 10 ; 
+    // davtalt 1: 123 4 sugarav
+    s = (s * 10) + digit ;
+    // 0 baigaa s deer 4 ochiv, 10 aar urjuulahed 40 +  2 dahi davtaltiin 3 = 43 
+    x = Math.floor(x / 10) ;
+    // 1234 iig 123.4 bolgov ? asuuh/ 
+} console.log ('bodlogo 1 urvuu too:', s)
+
+let x1 = 2345 ;
+let s1 = 0 ;
+
+while ( x1 !== 0 ) {
+ 
+    s1 = x1/1000 ;
+
+} console.log ('bodlogo 2 urvuu too :', s1) ;
+
+let x2 = 4;
+result = 0;
+let hurd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ;
+for ( i = 1 ; i <= 10 ; i++) {
+    result = x2 * hurd;
+} console.log (`4 hurd`, result);
+
+
+
+let x3 = 2;
+
+if ( x3 >= 2 || x3 / 2 === 0) {
+    console.log("not prime number", x3);
+} else if ( x3 <= 1 || x3 / 2 === 1 ) {
+    console.log ("prime number", x3) ;
+}
+
+let arr=[2,3,5,8,3] 
+let x4= 3;
+let count = 0;
+for ( i = 0 ; i < arr.length ; i++) {
+    if (arr[i] === x4) {
+    count = count + 1; 
+   }
+} console.log('x=3', count )
+
+let arr1 = [4, 5, 6, 7, 8] ;
+let digit2 = 0;
+let s2 = 0; 
+
+for ( let i = 0; i < arr1.length ; i ++ ) {
+    digit2 = arr1[i] % 2 ;
+    s2 = (s2 * 10) + arr1[i];   
+} console.log ('arr2 urvuu', s2);
+
+let arr2 = [30, 90, 10, 40, 20];
+let s3 = 0; 
